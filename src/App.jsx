f174026016288e4f3a9fe442fd81cd3d4b47f814
unknown <mac_johnson2@baylor.edu>
2025-06-09 13:31:58 -0500

    removed analytics

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,11 @@ import './App.css'
 import HomePage from "./HomePage";
 import Contact from "./Contact";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Analytics } from '@vercel/analytics/next';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Analytics />
         <Route path="/" element={<HomePage />} />
         <Route path="/contact" element={<Contact />} />
       </Routes>
